fcd4c767ed2a684b9fe6469da603c2a0b1f60b47
Dhaval Maniar <dhaval.maniar@nutanix.com>
2024-05-28 13:49:10 +0530

    Initial input box added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
 import logo from './logo.svg';
+// Import the style sheet.  You only need to do this one time in your project.
+import '@nutanix-ui/prism-reactjs/dist/index.css';
+import { Button, FlexLayout, Input, StackingLayout, Table } from '@nutanix-ui/prism-reactjs';
 import './App.css';
+import { useState } from 'react';
+
+
+const columns = [
+  {
+    title: "User Name",
+    key: "userName"
+  },
+  {
+    title: "Changes Count",
+    key: "changesCount"
+  }
+]
 
 function App() {
+
+  const [userName, setUserName] = useState('');
+  const [userData, setUserData] = useState([]);
+  const [loading, setLoading] = useState(false);  
+  
+  const handleUserNameChange = (e) => {
+    setUserName(e.target.value);
+  }
+
+  const getDataSource = () => {
+    return [];
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FlexLayout padding="20px" itemFlexBasis='100pc' flexDirection='column' alignItems='center' justifyContent='center'>
+      <FlexLayout>
+        <Input 
+          value={userName} 
+          placeholder='Enter username to get statistics' 
+          onChange={handleUserNameChange} 
+          label='Username'
+          style={{width: '300px'}}
+        />
+        <Button>Fetch</Button>
+      </FlexLayout>
+      {
+        userName &&
+        <Table
+          showCustomScrollbar={true}
+          dataSource={getDataSource()}
+          columns={columns}
+          topSection={{
+            title: "User Statistics",
+          }}
+          loading={loading} 
+          wrapperProps={{
+            "data-test-id": "rule-table",
+          }}
+          customMessages={{noData: "No User Data Found"}}
+        />  
+      }
+    </FlexLayout>
   );
 }
 
